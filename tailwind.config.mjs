9fbbca2f1c32ac1220128ca291ff339f71de2aa4
secona <77039267+secona@users.noreply.github.com>
2024-12-27 22:20:04 +0700

    feat: apply neobrutalism

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        hard: "0.2rem 0.2rem 0px 0px black",
+      }
+    },
   },
   plugins: [],
 };
