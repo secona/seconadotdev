81b333640c2bd0d052c8c327fb61a7245d331048
secona <secona00@gmail.com>
2025-06-28 11:38:39 +0700

    feat: update blog styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export default defineConfig({
   site: "https://secona.dev",
   integrations: [sitemap()],
+  markdown: {
+    shikiConfig: { theme: "catppuccin-mocha" },
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
+  },
 });
