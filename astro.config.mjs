81d335588385c48d0e9fccce6fd4bad813698fe6
secona <secona00@gmail.com>
2025-07-02 10:51:08 +0700

    feat(blog): add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import { toString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
 
 export default defineConfig({
   site: "https://secona.dev",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: { theme: "github-dark-high-contrast" },
+    remarkPlugins: [remarkReadingTime],
     rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
   },
 });
